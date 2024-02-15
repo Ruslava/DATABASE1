d775e827e440c28f52b1745f65bc2ca1a2ecbbda
Ruslava <152970376+Ruslava@users.noreply.github.com>
2024-02-15 20:36:32 +0100

    Update app.component.spec.ts

--- database/src/app/app.component.spec.ts
+++ database/src/app/app.component.spec.ts
@@ -14,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'database'`, () => {
+  it(`should have the 'database' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('database');
